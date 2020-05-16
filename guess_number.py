7496c73c83768e334663a1e92793f3a19dd3da6b
loverleyuan <33458051+loverleyuan@users.noreply.github.com>
2020-05-16 19:09:37 +0800

    猜数字

---
+++ guess_number.py
@@ -0,0 +1,20 @@
+# 2020-05-15 FRI
+# Author by toto
+from random import randint
+magic_num = randint(1, 5)
+input_times = 0
+bingo=False
+while bingo == False:
+    print(magic_num)
+    input_times += 1
+    input_num = input("Enter your lucky number (1...5):")
+    if input_num.isdigit():
+        if int(input_num) < magic_num:
+            print("Too small,please try again")
+        elif int(input_num) > magic_num:
+            print("Too big,please try again")
+        else:
+            print("bingo,count input_times %s"%input_times)
+            bingo = True
+    else:
+        print("User input need to be a number,please try again")
