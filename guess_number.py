20c7afdeb6b495269bdc98ba358a017d98d7095b
loverleyuan <33458051+loverleyuan@users.noreply.github.com>
2020-05-16 19:15:10 +0800

    调整范围

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,12 @@
 # 2020-05-15 FRI
 # Author by toto
 from random import randint
-magic_num = randint(1, 5)
+magic_num = randint(1, 100)
 input_times = 0
 bingo=False
 while bingo == False:
     input_times += 1
-    input_num = input("Enter your lucky number (1...5):")
+    input_num = input("Enter your lucky number (1...100):")
     if input_num.isdigit():
         if int(input_num) < magic_num:
             print("Too small,please try again")
